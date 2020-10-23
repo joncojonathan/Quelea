b0402ba2e72d9d8a9b441eb0f7ea8429a6f91b24
Jonathan Haddock <joncojonathan@gmail.com>
2020-10-23 09:55:08 +0100

    Import TextField

--- Quelea/src/main/java/org/quelea/windows/options/OptionsGeneralPanel.java
+++ Quelea/src/main/java/org/quelea/windows/options/OptionsGeneralPanel.java
@@ -27,6 +27,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
